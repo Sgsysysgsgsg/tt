846a347a4a5f7624d6f3df65a589f64d4b6fb2bf
Sgsysysgsgsg <106395544+Sgsysysgsgsg@users.noreply.github.com>
2022-09-07 09:51:25 +0200

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	// Rename this file to "config.js"
 
 	// Discord bot token
-	botToken: "OTk4NzQ2NTY4NDQ4NTQwNjgy.GJzBKV.ylg5gCVgobMYtLJnMSz2Nyt_d3tmNUnul8K_2k",
+	botToken: "",
 	// Link to bot avatar URL
 	botAvatarURL: "https://cdn.discordapp.com/attachments/1006410870898823250/1006644167046332466/20afc0ef44fcb3d98e5c7ae830985aba.png",
 	// Link to bot invite URL
